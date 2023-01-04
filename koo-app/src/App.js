afa87b92fe0859adc436be9685e8338b01ec1a6d
Pranav Purohit <adi089076@gmail.com>
2023-01-05 01:57:32 +0530

    updated UI

--- koo-app/src/App.js
+++ koo-app/src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
- 
+ import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
        <ChakraProvider>
-         
+         <Login/>
         </ChakraProvider> 
     </div>
   );
