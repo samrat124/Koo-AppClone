422708f359aefa33476b575f439f91cc12ae808e
samrat kumar <sam9999950@gmail.com>
2023-01-04 10:41:37 +0530

    homeNav

--- koo-app/src/Components/Home/NavHome.jsx
+++ koo-app/src/Components/Home/NavHome.jsx
@@ -4,7 +4,11 @@ import './NavHome.css'
 const NavHome = () => {
   return (
     <div>
-        
+      <div className='kooLogo'></div>  
+      <div className='homeNav'>
+        <div className='homeNavIcons'></div>
+        <div className='homeNavBarcode'></div>
+      </div>
     </div>
   )
 }
