1c9fd101325a6c2b5f5fa394d4766d52bcf9c999
rushikesh226 <rushikeshmali226@gmail.com>
2023-01-04 23:15:59 +0530

    follow

--- koo-app/src/Components/People/Components/FollowButton.jsx
+++ koo-app/src/Components/People/Components/FollowButton.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import {useState} from "react";
 import { Button } from '@chakra-ui/react'
 function FollowButton() {
+  const [follow,setFollow]=useState("+ Follow");
+  const [isFollowed,setisFollowed]=useState(false)
+  const handleFollow=()=>{
+    setisFollowed(!isFollowed);
+    setTimeout(()=>{
+      isFollowed ? setFollow("Unfollow") :setFollow(" + Follow")
+    },500)
+  }
   return (
-    <Button color="#fff" bg="#809b9d"  size="sm">+ Follow</Button>
+    <Button color="#fff" bg="#809b9d"  size="sm" onClick={handleFollow}>{follow}</Button>
   )
 }
 
