97b91d9a71a12f34b44f60ef57c399c22af230ee
sunaina <bawasunaina6@gmail.com>
2023-01-05 18:51:20 +0530

    done

--- koo-app/src/App.js
+++ koo-app/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-       
+      <BrowserRouter>
+       <Navbar/>
+       </BrowserRouter>
     </div>
   );
 }
