ff45cbdd733dcd410afbd9048a636d02736643fe
samrat kumar <sam9999950@gmail.com>
2023-01-06 16:29:11 +0530

    done

--- koo-app/src/App.js
+++ koo-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
        <ChakraProvider>
-        <Home/>
+        
         </ChakraProvider> 
     </div>
   );
--- koo-app/src/Components/Home/Home.jsx
+++ koo-app/src/Components/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import NavHome from './NavHome'
 import './Home.css'
-import { Button, Heading } from '@chakra-ui/react'
-import {AiOutlineArrowRight} from 'react-icons/ai'
+import { Button, Heading,Text,Box,Image } from '@chakra-ui/react'
+import {AiOutlineArrowRight} from 'react-icons/ai' 
+import HomeCarousel from './HomeCarousel'
+import kooAppqr from './kooAppqr.png'
+import kooFooter from './kooFooter.png'
 
 
 const Home = () => {
   return (
-    <div >
+    <div className='homeParent' >
       <div className='NavbarHome'><NavHome/></div>
        <div className='homeMain'>
         <div className='homeMainMid1'>
@@ -23,6 +26,29 @@ const Home = () => {
           <Heading size='2xl'>Features to explore</Heading>
           <p style={{marginTop:'30px'}}>Amazing features to connect with people</p>
         </div>
+        <HomeCarousel/>
+        <div className='homemidP3'>
+          <div className='homeMidp3p1'>
+            <Heading  >Koo in <br/>numbers</Heading>
+            <Text>Overwhelming response across the globe</Text>
+          </div>
+          <div className='homeMidp3p1'>
+            <Box h='400px' w='90%' borderRadius='20px'  margin='auto' display='grid'  gridTemplateColumns= 'repeat(2,1fr)' >
+            <Box  textAlign='center' borderRadius='10px'backgroundColor='white' boxShadow='1px 1px 5px grey'><Heading mt='20%'>100+</Heading> <br/>Countries</Box>
+            <Box  textAlign='center'   boxShadow='1px 1px 5px grey'><Heading mt='20%'>50M </Heading><br/>Downloads</Box>
+            <Box  textAlign='center'    boxShadow='1px 1px 5px grey'><Heading mt='20%'>7500+</Heading> <br/>Eminent Profiles</Box>
+            <Box  textAlign='center' borderRadius='10px'backgroundColor='white'   boxShadow='1px 1px 5px grey'><Heading mt='20%'>1000+</Heading><br/>Topics to explore</Box>
+            </Box>
+          </div>
+          
+        </div>
+        <div>
+            <Image src={kooAppqr} alt ='kooQr'/>
+          </div>
+          <div>
+            <Image src={kooFooter} alt ='kooQr'/>
+          </div>
+          <div className='langFoot'><Text >Available in English, हिन्दी, मराठी, தமிழ், తెలుగు, മലയാളം, ગુજરાતી, বাংলা, & many more languages coming soon...</Text></div>
       </div>
   )
 }
