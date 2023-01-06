3bf178ff1e0d7a2849424e4776d757a36c04c041
samrat kumar <sam9999950@gmail.com>
2023-01-06 21:52:51 +0530

    some error

--- koo-app/src/App.js
+++ koo-app/src/App.js
@@ -5,13 +5,15 @@ import NavHome from './Components/Home/NavHome';
 import Home from './Components/Home/Home'
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter } from 'react-router-dom';
+ 
 
 function App() {
   return (
     <div className="App">
-       <ChakraProvider>
+        <ChakraProvider>
+        <NavHome/>
         
-        </ChakraProvider> 
+        </ChakraProvider>
     </div>
   );
 }
