17208f636a48d995ce34e0e03c656d1b5553bff4
ramgopalshukla <gopalshukla28499@gmail.com>
2023-01-07 18:38:20 +0530

    post component added

--- koo-app/src/Components/Cricket/Cricket.js
+++ koo-app/src/Components/Cricket/Cricket.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Card from "../Card";
+import Card from "../Card/Card";
 
 export default function Cricket() {
     const [data, setdata] = useState([]);
