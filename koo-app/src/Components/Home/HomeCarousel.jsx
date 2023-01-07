82cf8a432e1efe02a4a1a792a3a6a5f1d185fa42
samrat kumar <sam9999950@gmail.com>
2023-01-07 14:06:34 +0530

    home

--- koo-app/src/Components/Home/HomeCarousel.jsx
+++ koo-app/src/Components/Home/HomeCarousel.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 // import Slider from 'infinite-react-carousel'; 
 import { Image } from '@chakra-ui/react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const HomeCarousel = () => {
   return (
-    <div style={{width:'100%',height:'600px' ,margin:'auto', }}>
-    {/* <Slider  slidesToShow={3} autoplay={true} nextArrow={null} >
+    <div style={{width:'100%',height:'600px' ,margin:'auto',display:'flex' }}>
+    <Carousel axis={horizontal}>
     <div>
        <Image w='80%' h='500px' margin='auto' src='https://images.kooapp.com/img/crsl-1.png' alt='carousel' />
     </div>
@@ -27,7 +29,8 @@ const HomeCarousel = () => {
     <div>
     <Image w='100%' h='500px'  margin='auto' src='https://images.kooapp.com/img/crsl-1.png' alt='carousel' />
     </div>
-  </Slider> */}
+    </Carousel>
+   
   </div>
   )
 }
