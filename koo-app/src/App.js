8a90135040637ccc8a42e865bde2227c4b91455d
samrat kumar <sam9999950@gmail.com>
2023-01-07 10:49:23 +0530

    some error fixed

--- koo-app/src/App.js
+++ koo-app/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from  "./Components/Navbar/Navbar.jsx"
+import Home from './Components/Home/Home'
 function App() {
   return (
     <div className="App">
        <BrowserRouter>
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Home/>
        </BrowserRouter>      
     </div>
   );
