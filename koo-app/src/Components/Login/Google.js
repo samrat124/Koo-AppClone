4b77bb0507f7d57eceb6407accd79bba706d284a
Pranav Purohit <adi089076@gmail.com>
2023-01-07 21:56:18 +0530

    updated

--- koo-app/src/Components/Login/Google.js
+++ koo-app/src/Components/Login/Google.js
@@ -7,6 +7,8 @@ import { Box, Input, Button, Text, Image } from "@chakra-ui/react"
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa"
+import Login from "./Login";
+import Dictaphone from "./mmm";
 
 function Search() {
 
@@ -41,20 +43,39 @@ function Search() {
 
     })
 
+
+
   }
+//   function record() {
+//     var recognition = new webSpeechRecognition();
+//     recognition.lang = "en-GB";
+
+//     recognition.onresult = function(event) {
+//         // console.log(event);
+//         console.log(event)
+//         setState(event.results[0][0].transcript);
+//     }
+//     recognition.start();
+
+// }
+const valueText=(value)=>{
+setState(value);
+}
+
+   
+  
 
 
   return (
     <div>
 
-      <Box display="flex" alignItems="center" w="90%" margin="auto" ><FaSearch style={{ marginRight: "-35px" }} /><Input textAlign="center" borderRadius="20px" placeholder="Search" type="text" autoFocus value={state} onChange={(event) => {
-        setState(event.target.value);
-      }} /></Box>
+      <Dictaphone onChange1={valueText} state={state} />
       {state !== "" ? <Box display="flex" flexDirection="column" gap="0" w="90%" margin="auto" height="600px" bg="#F0FFF0" >
         {arr.map((ele) => {
           return <Text display="flex" alignItems="center" gap="5" borderRadius="20px" p="5"  ><Image w="10%" borderRadius="20px" src={ele.image} alt="one" /><Link to={`/trending/${ele.id}`}><h1><b>{ele.name}</b></h1><p style={{ fontSize: "14px" }}>{ele.username}</p></Link></Text>
         })}
       </Box> : null}
+      {/* <button onClick={()=>record()}>lo</button> */}
       
 
     </div>
