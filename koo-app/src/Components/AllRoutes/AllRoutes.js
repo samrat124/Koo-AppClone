ae3825e1273b5c2fcc141a069a368a034e69e638
Pranav Purohit <adi089076@gmail.com>
2023-01-07 14:03:48 +0530

    updated

--- koo-app/src/Components/AllRoutes/AllRoutes.js
+++ koo-app/src/Components/AllRoutes/AllRoutes.js
@@ -1,9 +1,32 @@
 import React from 'react'
-import {Routes,Route} from'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Cricket from '../Cricket/Cricket'
+import Home from '../Home/Home'
+import Navbar from '../Navbar/Navbar'
+import Feed from '../Feed/Feed'
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <div>
+
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/navbar" element={<Navbar />}>
+
+          <Route path="/cricket" element={<Cricket />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/people" element={<People />} />
+
+
+        </Route>
+
+
+
+      </Routes>
+
+    </div>
   )
 }
 
