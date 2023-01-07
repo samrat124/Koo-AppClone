a70099efb3214e90c04c4e70ecef26134d86f688
Pranav Purohit <adi089076@gmail.com>
2023-01-07 14:38:38 +0530

    updated

--- koo-app/src/App.js
+++ koo-app/src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from  "./Components/Navbar/Navbar.jsx"
+import Login from "./Components/Login/Login"
+import AllRoutes from './Components/AllRoutes/AllRoutes';
 function App() {
   return (
     <div className="App">
        <BrowserRouter>
-      <Navbar/>
+
+       {/* <Navbar/> */}
+       <AllRoutes/>
+       {/* <Login/> */}
+      
        </BrowserRouter>      
     </div>
   );
