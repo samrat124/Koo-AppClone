90c37c6cdaca1353ac9a276dc0236c05f44bfb44
ramgopalshukla <gopalshukla28499@gmail.com>
2023-01-08 16:07:14 +0530

    cricket like

---
+++ koo-app/src/Components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './loading.css'
+export default function loading() {
+  return (
+    <div className="g-loading-gif">
+        <div>
+        <img src="https://cdn.dribbble.com/users/1787505/screenshots/7300251/media/a351d9e0236c03a539181b95faced9e0.gif" alt="" />
+        </div>
+      </div>
+  )
+}
