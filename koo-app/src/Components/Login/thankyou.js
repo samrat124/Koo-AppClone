41203d71edc0567acc394650e90341b27308bcd0
Pranav Purohit <adi089076@gmail.com>
2023-01-09 17:49:01 +0530

    updated

---
+++ koo-app/src/Components/Login/thankyou.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button } from '@chakra-ui/react';
+import { useState } from 'react';
+import ReactAudioPlayer from 'react-audio-player';
+
+function Thankyou() {
+    let [sound,setSound]=useState(false);
+  return (
+    <div>
+    {sound?<ReactAudioPlayer style={{position:"absolute",top:"-500px"}}
+                src="/Thankyou,We are Open For your Suggestion..mp3"
+                autoPlay={true}
+                controls
+
+            />:null} 
+
+    <Button bg="blue" colorScheme="blue" w="50%" onClick={()=>{
+        setSound(!sound);
+    }} >
+Thankyou
+ 
+    </Button>
+    </div>
+  )
+}
+
+export default Thankyou
